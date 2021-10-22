997ad4fd00942a6ca42859a1468b94f95d11a035
Shavin Croos <t.s.croos@student.reading.ac.uk>
2021-10-22 11:02:45 +0100

    Built Basic Menu and 3rd Update README.md

--- Emotion Recognition System.py
+++ Emotion Recognition System.py
@@ -1,4 +1,28 @@
 import cv2
 import numpy as np
+import time
+from tkinter import *
 
-print('This is the start of my Emotion-Based Vision Detection application.')
+win = Tk()
+win.geometry('500x500')
+win.title("ERS Visual V1.0")
+Label(win, text = "ERS VISUAL V1.0\n", font = 'arial 20 bold').pack()
+Label(win, text = "Emotion Recognition System", font = 'arial 20 bold').place(x = 50, y = 45)
+Label(win, text = 'MENU', font = 'arial 15 bold').place(x = 210, y = 90)
+
+def emotRegSys():
+    winERS = Tk()
+    winERS.geometry('500x500')
+    winERS.title('ERS Visual V1.0 - ERS')
+    Button(winERS, text = 'QUIT', font = 'arial 15', command = quit, bg = 'grey').place(x = 200, y = 140)
+
+def credit():
+    winCredit = Tk()
+    winCredit.geometry('500x500')
+    winCredit.title('ERS Visual V1.0 - Credits')
+
+    Label(winCredit, text = "CREDITS", font = 'arial 20 bold').pack()
+
+Button(win, text = 'START', font = 'arial 15', command = emotRegSys, bg = '#4dff00').place(x = 200, y = 140)
+Button(win, text = 'CREDITS', font = 'arial 15', command = credit, bg = 'cyan').place(x = 188, y = 210)
+Button(win, text = 'QUIT', font = 'arial 15', command = quit, bg = '#ff2626').place(x = 210, y = 280)
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This is my individual project for vision-based emotion detection.
 3 October 2021 - As of currently, the application has not been made yet.
 
 15 October 2021 - Have started working on creating the application for the emotion recognition software for the final year project.
+
+22 October 2021 - Have got a basic menu setup completed where the user can select one of 3 options in the software, which are to run the ERS itself, view the credits of the application and quit the application entirely. Upon clicking the run button for the ERS, the user will be taken to another window, which is currently blank at the moment. The user can now quit the entire application from this window as well.

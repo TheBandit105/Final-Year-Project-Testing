617b7cf15923ad235911e7add1c739251b8e1dfb
Shavin Croos <t.s.croos@student.reading.ac.uk>
2022-01-23 00:33:34 +0000

    Added Emotion Detection and 5th Update README.md

--- Emotion Recognition System.py
+++ Emotion Recognition System.py
@@ -1,11 +1,10 @@
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 import time
 from tkinter import *
 from deepface import DeepFace
 
 cam = cv2.VideoCapture(0)
+
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 win = Tk()
@@ -18,6 +17,7 @@ Label(win, text = 'MENU', font = 'arial 15 bold').place(x = 210, y = 90)
 def emotRegSys():
     while True:
         ret, frame = cam.read()
+        emotRes = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection = False)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(gray, 1.1, 4)
@@ -27,7 +27,14 @@ def emotRegSys():
 
         font = cv2.FONT_HERSHEY_SIMPLEX
 
-        cv2.putText(frame, "FPS: {}".format(cam.get(cv2.CAP_PROP_FPS)), (10,30), font, 1, (0, 255, 255), 2, cv2.LINE_4) 
+        cv2.putText(frame, 'Angry: ' + str('%.8f' % (emotRes['emotion'].get('angry'))), (10, 20), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Fear: ' + str('%.8f' % (emotRes['emotion'].get('fear'))), (10, 40), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Neutral: ' + str('%.8f' % (emotRes['emotion'].get('neutral'))), (10, 60), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Sad: ' + str('%.8f' % (emotRes['emotion'].get('sad'))), (10, 80), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Disgust: ' + str('%.8f' % (emotRes['emotion'].get('disgust'))), (10, 100), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Happy: ' + str('%.8f' % (emotRes['emotion'].get('happy'))), (10, 120), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+        cv2.putText(frame, 'Surprise: ' + str('%.8f' % (emotRes['emotion'].get('surprise'))), (10, 140), font, 0.5, (0, 255, 255), 2, cv2.LINE_4)
+
         cv2.putText(frame, 'Press q to quit', (200,450), font, 1, (0, 255, 255), 2, cv2.LINE_4)
         cv2.imshow("ERS Visual V1.0 - Emotion Recognition System", frame)
 
@@ -44,9 +51,22 @@ def credit():
     winCredit.title('ERS Visual V1.0 - Credits')
 
     Label(winCredit, text = "CREDITS", font = 'arial 20 bold').pack()
-    Label(winCredit, text = "Created and Tested by Shavin Croos", font = 'arial 10').pack()
-    
+    Label(winCredit, text = "Created and Tested by Shavin Croos", font = 'arial 15').pack()
 
-Button(win, text = 'START', font = 'arial 15', command = emotRegSys, bg = '#4dff00').place(x = 200, y = 140)
-Button(win, text = 'CREDITS', font = 'arial 15', command = credit, bg = 'cyan').place(x = 188, y = 210)
-Button(win, text = 'QUIT', font = 'arial 15', command = quit, bg = '#ff2626').place(x = 210, y = 280)
+    Label(winCredit, text = "Libraries Used: ", font = 'arial 15').pack()
+    Label(winCredit, text = "OpenCV", font = 'arial 15').pack()
+    Label(winCredit, text = "Deepface", font = 'arial 15').pack()
+    Label(winCredit, text = "Tkinter", font = 'arial 15').pack()
+
+def about():
+    winAbout = Tk()
+    winAbout.geometry('500x500')
+    winAbout.title('ERS Visual V1.0 - About')
+
+    Label(winAbout, text = "ABOUT", font = 'arial 20 bold').pack()
+    
+Button(win, text = 'START (INTERNAL CAM)', font = 'arial 15', command = emotRegSys, bg = '#4dff00').place(x = 125, y = 140)
+Button(win, text = 'START (USB CAM)', font = 'arial 15', bg = '#4dff00').place(x = 150, y = 200)
+Button(win, text = 'ABOUT', font = 'arial 15', command = about, bg = 'yellow').place(x = 198, y = 260)
+Button(win, text = 'CREDITS', font = 'arial 15', command = credit, bg = 'cyan').place(x = 188, y = 320)
+Button(win, text = 'QUIT', font = 'arial 15', command = quit, bg = '#ff2626').place(x = 210, y = 380)
